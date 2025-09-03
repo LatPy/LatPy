6ebb96935f4b8165fef70a138c19bd3cad9ef34f
Nagato <transcendentalliouville@gmail.com>
2025-09-03 21:06:02 +0900

    Added class LatPy

--- latpy/__init__.py
+++ latpy/__init__.py
@@ -1,5 +1,4 @@
-from .loader import lib
-from latpy import latpy
+from latpy.latpy import LatPy
 from .core import compute_gso, volume, sl, pot
 
-__all__ = ["lib", "compute_gso", "volume", "sl", "pot"]
+__all__ = ["lib", "compute_gso", "volume", "sl", "pot", "LatPy"]
--- latpy/latpy.py
+++ latpy/latpy.py
@@ -1,1 +1,40 @@
-from .loader import lib
+from . import core
+import numpy as np
+
+class LatPy:
+    """
+    A class for representing a lattice in n-dimensional space.
+    """
+    def __init__(self, basis):
+        self.basis = basis
+        self.n, self.m = basis.shape
+
+    def __repr__(self):
+        return f"LatPy(basis={self.basis})"
+
+    def __str__(self):
+        return f"{self.n}-dimensional lattice with basis:\n{self.basis}"
+
+    def compute_gso(self) -> tuple[np.ndarray[float], np.ndarray[float]]:
+        """Compute the Gram-Schmidt orthogonalization of the lattice basis.
+
+        Returns:
+            np.ndarray: The GSO basis.
+        """
+        return core.compute_gso(self.basis)
+
+    def volume(self) -> int:
+        """Calculate the volume of the lattice.
+
+        Returns:
+            int: The volume of the lattice.
+        """
+        return core.volume(self.basis)
+    
+    def sl(self) -> float:
+        """Calculate the GSA-slope of the lattice.
+
+        Returns:
+            float: The GSA-slope of the lattice basis.
+        """
+        return core.sl(self.basis)
